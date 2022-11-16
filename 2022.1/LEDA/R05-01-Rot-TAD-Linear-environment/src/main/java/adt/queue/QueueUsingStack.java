8d616d93611811bd9288dff2656001ce32677dbf
Jônatas Tavares <jonatas.santos@ccc.ufcg.edu.br>
2022-11-16 17:21:49 -0300

    Queue Using Stack

--- 2022.1/LEDA/R05-01-Rot-TAD-Linear-environment/src/main/java/adt/queue/QueueUsingStack.java
+++ 2022.1/LEDA/R05-01-Rot-TAD-Linear-environment/src/main/java/adt/queue/QueueUsingStack.java
@@ -2,6 +2,8 @@ package adt.queue;
 
 import adt.stack.Stack;
 import adt.stack.StackImpl;
+import adt.stack.StackOverflowException;
+import adt.stack.StackUnderflowException;
 
 public class QueueUsingStack<T> implements Queue<T> {
 
@@ -17,28 +19,59 @@ public class QueueUsingStack<T> implements Queue<T> {
 	public void enqueue(T element) throws QueueOverflowException {
 		if (this.isFull())
 			throw new QueueOverflowException();
+		try {
+			if (element != null)
+				this.stack1.push(element);
+		} catch (StackOverflowException e) {
+			throw new QueueOverflowException();
+		}
 	}
 
 	@Override
 	public T dequeue() throws QueueUnderflowException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		T dequeue = null;
+		if (this.isEmpty())
+			throw new QueueUnderflowException();
+		try {
+			if (this.stack2.isEmpty()) {
+				while (!this.stack1.isEmpty()) {
+					dequeue = this.stack1.pop();
+					this.stack2.push(dequeue);
+				}
+			}
+			dequeue = this.stack2.top();
+			this.stack2.pop();
+		} catch (StackUnderflowException | StackOverflowException e) {
+
+		}
+		return dequeue;
 	}
 
 	@Override
 	public T head() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		T head = null;
+		if (this.stack2.isEmpty()) {
+			try {
+				while (!this.stack1.isEmpty()) {
+					this.stack2.push(this.stack1.top());
+					this.stack1.pop();
+				}
+				head = this.stack2.top();
+			} catch (StackOverflowException | StackUnderflowException e) {
+
+			}
+		}
+		return head;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return this.stack1.isEmpty();
+		return this.stack1.isEmpty() && this.stack2.isEmpty();
 	}
 
 	@Override
 	public boolean isFull() {
-		return this.stack1.isFull();
+		return this.stack1.isFull() || this.stack2.isFull();
 	}
 
 }
