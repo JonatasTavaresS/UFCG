96fc07e38b5f6fc67ae77dc5c1812c996f75a205
Jônatas Tavares <jonatas.santos@ccc.ufcg.edu.br>
2022-12-09 10:30:35 -0300

    Reorganização de diretórios

---
+++ 2022.1/LEDA/README.md
@@ -0,0 +1,8 @@
+# 💻 Laboratório de Estrutura de Dados e Algoritmos
+
+Implementações de roteiros e provas desenvolvidas na disciplina de Laboratório de Estrutura de Dados e Algoritmos ministrada no período 2022.1 pelo professor Dr. Adalberto Cajueiro de Farias, durante o 3º semestre do curso de Ciência da Computação na UFCG.
+
+- [Provas Práticas](/Provas%20Pr%C3%A1ticas/)
+- [Roteiros](/Roteiros/)
+- [Roteiros de Revisão](/Roteiros%20de%20Revis%C3%A3o/)
+- [Roteiros Especiais](/Roteiros%20Especiais/)
---
+++ 2022.1/LEDA/Roteiros Especiais/RE5-01-RE-Linked-list-iterativa-ordenada-environment/src/main/java/adt/linkedList/SingleLinkedListImpl.java
@@ -0,0 +1,59 @@
+package adt.linkedList;
+
+public class SingleLinkedListImpl<T> implements LinkedList<T> {
+
+	protected SingleLinkedListNode<T> head;
+
+	public SingleLinkedListImpl() {
+		this.head = new SingleLinkedListNode<T>();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.getHead().isNIL();
+	}
+
+	@Override
+	public int size() {
+		int size = 0;
+		SingleLinkedListNode<T> aux = this.getHead();
+		while (!aux.isNIL()) {
+			size++;
+			aux.getNext();
+		}
+		return size;
+	}
+
+	@Override
+	public T search(T element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented!");
+	}
+
+	@Override
+	public void insert(T element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented!");
+	}
+
+	@Override
+	public void remove(T element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented!");
+	}
+
+	@Override
+	public T[] toArray() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented!");
+	}
+
+	public SingleLinkedListNode<T> getHead() {
+		return head;
+	}
+
+	public void setHead(SingleLinkedListNode<T> head) {
+		this.head = head;
+	}
+
+}
